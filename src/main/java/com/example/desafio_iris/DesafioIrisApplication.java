48b5067a0de7df7bdcf68e08f5c98da05f712d7c
marlonszpak <marloszpaik@gmail.com>
2024-07-14 15:06:39 -0300

    Corrigindo bugs do deploy 3

--- src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
+++ src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
@@ -3,8 +3,10 @@ package com.example.desafio_iris;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}, scanBasePackages={"com.example.desafio_iris"})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories
 public class DesafioIrisApplication {
 
 	public static void main(String[] args) {
