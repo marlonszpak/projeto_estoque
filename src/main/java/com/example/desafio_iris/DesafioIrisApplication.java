8c2d5004fa654738aa3d17c8abc378a78bf00490
marlonszpak <marloszpaik@gmail.com>
2024-07-14 15:21:28 -0300

    Corrigindo bugs do deploy 5

--- src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
+++ src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 @EnableJpaRepositories
 public class DesafioIrisApplication {
 
