293bd49508e8fd6a023ab63c5b5c3f45fbc3f1f0
marlonszpak <marloszpaik@gmail.com>
2024-07-14 14:10:29 -0300

    Corrigindo bugs do deploy 2

--- src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
+++ src/main/java/com/example/desafio_iris/DesafioIrisApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}, scanBasePackages={"com.example.desafio_iris"})
 public class DesafioIrisApplication {
 
 	public static void main(String[] args) {
